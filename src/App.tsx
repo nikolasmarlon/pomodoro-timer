9e3d7eb80429113796cc21ca0cde6ea7d6736d0c
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2022-07-28 00:47:29 -0300

    React-router-dom Rotas no React

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variantColor="primary" />
-      <Button variantColor="secondary" />
-      <Button variantColor="danger" />
-      <Button variantColor="success" />
-      <Button />
-      asdfasdf
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer } from './Button.styles'
-
-interface ButtonProps {
-  variantColor?: 'primary' | 'secondary' | 'danger' | 'success'
-}
-
-export function Button({ variantColor = 'primary' }: ButtonProps) {
-  return <ButtonContainer variantColor={variantColor}>Enviar</ButtonContainer>
-}
